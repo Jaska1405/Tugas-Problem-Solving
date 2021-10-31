5e9c98b08485906c14e479cd767b30130e9ccb38
Jaska1405 <90269301+Jaska1405@users.noreply.github.com>
2021-10-31 22:43:11 +0700

    Main

---
+++ Assets/Scripts/Box.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Box : MonoBehaviour
+{
+    public float Health = 50f;
+    public UnityAction<GameObject> OnBoxDestroyed = delegate { };
+
+    private bool _isHit = false;
+
+    void OnDestroy()
+    {
+        if (_isHit)
+        {
+            OnBoxDestroyed(gameObject);
+            //Spawner1.IncreaseScore();
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.GetComponent<Rigidbody2D>() == null) return;
+
+        if (col.gameObject.tag == "Ball")
+        {
+            _isHit = true;
+            Destroy(gameObject);
+            ScoreManager.scoreValue += 1;
+        }
+
+        else if (col.gameObject.tag == "Bullet")
+        {
+            float damage = col.gameObject.GetComponent<Rigidbody2D>().velocity.magnitude * 10;
+            Health -= damage;
+
+            if (Health <= 0)
+            {
+                _isHit = true;
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnGenerate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnGenerate : MonoBehaviour
+{
+    public GameObject Box;
+    public float SpawnRate = 0f;
+    public float nextSpawn = 0f;
+    float randomX;
+    int randomY;
+    private float[] linepos = { 3.85f, 2.42f, 2.13f, 1.51f, -3.30f };
+    Vector2 whereToSpawn;
+
+    void Update()
+    {
+        if (Time.time > nextSpawn)
+        {
+            randomX = Random.Range(-6f, 6f);
+            randomY = Random.Range(0, linepos.Length);
+            whereToSpawn = new Vector2(randomX, linepos[randomY]);
+            Instantiate(Box, whereToSpawn, Quaternion.identity);
+            nextSpawn = Time.time + SpawnRate;
+            Debug.Log(Time.time + SpawnRate);
+        }
+    }
+}
